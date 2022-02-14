21059b8601d709c90fdd4151205a5328cdfa9963
sebhanika <hanikasebastian@gmail.com>
2022-02-14 18:37:57 +0100

    Final edits

--- webmap_R.R
+++ webmap_R.R
@@ -1,8 +1,11 @@
 # Webmap tutorial
 # Code based on tutorial by Helen Mckenzie
 # https://www.helenmakesmaps.com/
-# Here I will present municipal employment growth in German municipaliteis
+# Here I will present municipal employment growth in German municipalities
 # Data from the Federal Employment Agency and part of my Thesis.
+
+
+# 1. Libraries -------------------------------------------------------------
 
 library(tidyverse)
 library(leaflet)
@@ -10,6 +13,8 @@ library(rgdal)
 library(readxl)
 library(RColorBrewer)
 
+
+# 2. Data import and cleaning ---------------------------------------------
 
 #download List of all German municipalities
 url_muni <- "https://www.destatis.de/DE/Themen/Laender-Regionen/Regionales/Gemeindeverzeichnis/Administrativ/Archiv/GVAuszugJ/31122020_Auszug_GV.xlsx?__blob=publicationFile"
@@ -27,8 +32,7 @@ muni_list <- read_xlsx(path = "31122020_Auszug_GV.xlsx",
   select(-c(2:5)) %>%
   rename("name" = ...6)
 
-
-# read in geoJson
+### read in geoJson employment data
 data.empgrw.raw <- readOGR("webmap_data/emp_grw_clean.geojson")
 
 # join data
@@ -38,6 +42,15 @@ data.empgrw$emp_grw_p <- round(data.empgrw$emp_grw_p, 2)
 # filter for municipalities in Hesse and Thruinga
 data.empgrw.HT <- subset(data.empgrw, str_detect(ags ,"^06|^16"))
 
+### read in geoJson for State borders
+# important that it is only polylines, otherwise the code below does not work
+data.laender.raw <- readOGR("webmap_data/l_bundeslaender.geojson")
+
+# filter for Hesse and Thuringa
+data.laender.HT <- subset(data.laender.raw, str_detect(AGS ,"^06|^16"))
+
+
+# 3. Setting colors and map features --------------------------------------
 
 # creating custom color palette
 self.palette <- c("#cc99ff","#C2E699", "#78C679", "#31A354","#006837")
@@ -50,28 +63,40 @@ data.pal <- colorBin(palette = self.palette,
                      bins = data.bins)
 
 
-#SPecifiy what should be shown when clicking on municipalityup content
+#Specify what should be shown when clicking on municipality up content
 data.empgrw.HT$popup <- paste("<strong>", data.empgrw.HT$name,"</strong>", "</br>", 
                                data.empgrw.HT$ags, "</br>", # municipal key
                            "Employment growth in %", 
                            prettyNum(data.empgrw.HT$emp_grw_p, big.mark = ","))
 
+
+# 4. Create webmap --------------------------------------------------------
+
 # create webmap
 web.map <- leaflet() %>%
   addProviderTiles(providers$CartoDB.Positron) %>% 
-  addPolygons(data = data.empgrw.HT,
+  # polygons of Municipalities with Employment Growth data
+  addPolygons(data = data.empgrw.HT, 
               stroke = TRUE,
               weight = 0.1,
               color = "#ABABAB",
               smoothFactor = 0.3,
               opacity = 0.9, # of stroke
               fillColor = ~data.pal(data.empgrw.HT$emp_grw_p),
-              fillOpacity = 0.6,
+              fillOpacity = 0.8,
               popup = ~popup,
-              highlightOptions = highlightOptions(color = "#E2068A", # hihllighs borders when hovering
+              highlightOptions = highlightOptions(color = "#E2068A", # highlights borders when hovering
                                                   weight = 1.5,
                                                   bringToFront = TRUE,
                                                   fillOpacity = 0.5)) %>% 
+  # polyglines of State Borders
+  addPolylines(data = data.laender.HT, 
+               stroke = TRUE,
+               weight = 0.7,
+               color = "#585858",
+               smoothFactor = 0.3,
+               opacity = 1) %>% 
+  
   addLegend(position = "bottomright", #adding legend
             opacity = 0.9,  
             title = "Employment Growth </br>(2009 to 2019)",
@@ -88,4 +113,7 @@ web.map <- leaflet() %>%
 
 # call web.map
 web.map
+
+
+
 
